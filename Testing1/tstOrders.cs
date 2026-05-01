9a6eeadafa48ed3a05a754a57df96e2ecd258e6e
P2892889 <P2892889@my365.dmu.ac.uk>
2026-05-01 10:54:22 +0100

    Test for Dennis

---
+++ Testing1/tstOrders.cs
@@ -0,0 +1,19 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing1
+{
+    [TestClass]
+    public class tstOrders
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            //test to verify it exists
+            Assert.IsNotNull(AnOrder);
+        }
+    }
+}
