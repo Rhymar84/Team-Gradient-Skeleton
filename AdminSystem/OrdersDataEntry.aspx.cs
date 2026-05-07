0b16fca07858f25343b2af60b62ccfe3f4cb8120
P2892889 <P2892889@my365.dmu.ac.uk>
2026-05-07 10:25:41 +0100

    Added data entry and viewer pages for orders.

--- AdminSystem/OrdersDataEntry.aspx.cs
+++ AdminSystem/OrdersDataEntry.aspx.cs
@@ -4,11 +4,30 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+
+    protected void btnOK_Click(object sender, EventArgs e)
+    {
+        //create instance of clsOrder
+        clsOrder AnOrder = new clsOrder();
+        //capture data
+        AnOrder.OrderNo = Convert.ToInt32(txtOrderNo.Text);
+        AnOrder.DateOrdered = DateTime.Now;
+        AnOrder.ShippingAddress = txtShippingAddress.Text;
+        AnOrder.OrderStatus = "ordered";
+        AnOrder.DeliveryInstructions = txtDeliveryInstructions.Text;
+        AnOrder.ExpressShipping = chkExpressShipping.Checked;
+        AnOrder.SubTotal = Convert.ToSingle(txtSubTotal.Text);
+        //store the order in session object
+        Session["AnOrder"] = AnOrder;
+        //navigate to view page
+        Response.Redirect("OrdersViewer.aspx");
     }
 }
--- AdminSystem/OrdersViewer.aspx.cs
+++ AdminSystem/OrdersViewer.aspx.cs
@@ -4,11 +4,23 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        //create new clsOrder
+        clsOrder AnOrder = new clsOrder();
+        //get data from session object
+        AnOrder = (clsOrder)Session["AnOrder"];
+        //display data
+        Response.Write("<b>OrderNo: </b>" + AnOrder.OrderNo + "<br>");
+        Response.Write("<b>Date Ordered: </b>" + AnOrder.DateOrdered + "<br>");
+        Response.Write("<b>Shipping Address: </b>" + AnOrder.ShippingAddress + "<br>");
+        Response.Write("<b>Order Status: </b>" + AnOrder.OrderStatus + "<br>");
+        Response.Write("<b>Delivery Instructions: </b>" + AnOrder.DeliveryInstructions + "<br>");
+        Response.Write("<b>Express Shipping: </b>" + AnOrder.ExpressShipping + "<br>");
+        Response.Write("<b>Subtotal: </b>£" + AnOrder.SubTotal + "<br>");
     }
 }
