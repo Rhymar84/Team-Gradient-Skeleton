7089831dd982ec05358754754bbe8930609ad259
P2892889 <P2892889@my365.dmu.ac.uk>
2026-05-07 11:02:11 +0100

    Patch to fix tests

--- Testing1/tstOrders.cs
+++ Testing1/tstOrders.cs
@@ -77,7 +77,7 @@ namespace Testing1
         public void SubTotalPropertyOK()
         {
             clsOrder AnOrder = new clsOrder();
-            decimal TestData = 789.99;
+            decimal TestData = 789.99M;
             AnOrder.SubTotal = TestData;
             Assert.AreEqual(AnOrder.SubTotal, TestData);
         }
