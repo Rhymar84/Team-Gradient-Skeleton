5391fbb8b380a9a254a1c539b2e186a1e8c49401
P2804412 <P2804412@my365.dmu.ac.uk>
2026-05-14 09:55:00 +0100

    Added all test for attributes

--- ClassLibrary/clsStock.cs
+++ ClassLibrary/clsStock.cs
@@ -6,5 +6,16 @@ namespace ClassLibrary
     {
         public bool Active { get; set; }
         public DateTime DateAdded { get; set; }
+        public int ItemNo { get; set; }
+        public string ModelName { get; set; }
+        public string Price { get; set; }
+        public int Quantity { get; set; }
+        public bool InStock { get; set; }
+        public DateTime LastDateRestocked { get; set; }
+
+        public bool Find(int itemNo)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Testing2/tstStock.cs
+++ Testing2/tstStock.cs
@@ -11,70 +11,61 @@ namespace Testing2
         public void InstanceOK()
         {
             clsStock anStock = new clsStock();
-
             Assert.IsNotNull(anStock);
         }
 
         [TestMethod]
-        public void ActivePropertyOK()
-        {
-            clsStock anStock = new clsStock();
-            Boolean TestData = true;
-            anStock.Active = TestData;
-            Assert.AreEqual(anStock.Active, TestData);
-        }
-
-        [TestMethod]
-        public void DateAddedPropertyOK()
-        {
-            clsStock anStock = new clsStock();
-            DateTime TestData = DateTime.Now.Date;
-            anStock.DateAdded = TestData;
-            Assert.AreEqual(anStock.DateAdded, TestData);
-        }
-        [TestMethod]
-
-        public void StockIDPropertyOK()
+        public void ItemNoPropertyOK()
         {
             clsStock anStock = new clsStock();
             Int32 TestData = 1;
-            anStock.StockID = TestData;
-            Assert.AreEqual(anStock.StockID, TestData);
+            anStock.ItemNo = TestData;
+            Assert.AreEqual(anStock.ItemNo, TestData);
         }
 
         [TestMethod]
-
-        public void StockNamePropertyOK()
+        public void ModelNamePropertyOK()
         {
             clsStock anStock = new clsStock();
-            string TestData = "Test Stock Name";
-            anStock.StockName = TestData;
-            Assert.AreEqual(anStock.StockName, TestData);
+            string TestData = "Test Model Name";
+            anStock.ModelName = TestData;
+            Assert.AreEqual(anStock.ModelName, TestData);
         }
 
         [TestMethod]
-
-        public void StockPricePropertyOK()
+        public void PricePropertyOK()
         {
             clsStock anStock = new clsStock();
-            decimal TestData = 9.99m;
-            anStock.StockPrice = TestData;
-            Assert.AreEqual(anStock.StockPrice, TestData);
+            string TestData = "9.99";
+            anStock.Price = TestData;
+            Assert.AreEqual(anStock.Price, TestData);
         }
 
         [TestMethod]
-
-        public void FindMethodOK()
+        public void QuantityPropertyOK()
         {
             clsStock anStock = new clsStock();
-            Boolean Found = false;
-            Int32 StockID = 1;
-            Found = anStock.Find(StockID);
-            Assert.IsTrue(Found);
+            Int32 TestData = 100;
+            anStock.Quantity = TestData;
+            Assert.AreEqual(anStock.Quantity, TestData);
         }
 
         [TestMethod]
+        public void InStockPropertyOK()
+        {
+            clsStock anStock = new clsStock();
+            Boolean TestData = true;
+            anStock.InStock = TestData;
+            Assert.AreEqual(anStock.InStock, TestData);
+        }
 
+        [TestMethod]
+        public void LastDateRestockedPropertyOK()
+        {
+            clsStock anStock = new clsStock();
+            DateTime TestData = DateTime.Now.Date;
+            anStock.LastDateRestocked = TestData;
+            Assert.AreEqual(anStock.LastDateRestocked, TestData);
+        }
+        }
     }
-}
-
