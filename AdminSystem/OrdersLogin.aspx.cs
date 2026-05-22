9e54180cf986158f6a022abc8494d22d10e1127f
P2892889 <P2892889@my365.dmu.ac.uk>
2026-05-22 10:19:22 +0100

    Added basic orders Login.

---
+++ AdminSystem/OrdersLogin.aspx.cs
@@ -0,0 +1,43 @@
+﻿using ClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class OrdersLogin : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnLogin_Click(object sender, EventArgs e)
+    {
+        clsOrdersUser AUser = new clsOrdersUser();
+        string UserName = txtUserName.Text;
+        string Password = txtPassword.Text;
+
+        if (txtUserName.Text == "") //No username error
+        {
+            lblError.Text = "You must enter a username.";
+            return;
+        }
+        if (txtPassword.Text == "") //No password error
+        {
+            lblError.Text = "You must enter a password.";
+            return;
+        }
+
+        if (AUser.FindUser(UserName, Password)) //find record
+        {
+            Response.Redirect("OrdersList.aspx");
+        } 
+        else // record not found
+        {
+            lblError.Text = "Login details are incorrect. Please try again.";
+        }
+
+    }
+}
---
+++ ClassLibrary/clsOrdersUser.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class clsOrdersUser
+    {
+        public int UserID { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Department { get; set; }
+
+        public bool FindUser(string userName, string password)
+        {
+            clsDataConnection DB = new clsDataConnection();
+            DB.AddParameter("@UserName", userName);
+            DB.AddParameter("@Password", password);
+            DB.Execute("sproc_tblUsers_FindUserNamePW");
+
+            //should be 1 or 0 records
+            if (DB.Count == 1)
+            {
+                UserID = Convert.ToInt32(DB.DataTable.Rows[0]["UserID"]);
+                UserName = Convert.ToString(DB.DataTable.Rows[0]["UserName"]);
+                Password = Convert.ToString(DB.DataTable.Rows[0]["Password"]);
+                Department = Convert.ToString(DB.DataTable.Rows[0]["Department"]);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
