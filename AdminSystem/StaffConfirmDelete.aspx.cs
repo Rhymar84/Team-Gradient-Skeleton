c0dad9b02bcf686ab9f5c5174cbb86f0829fce2c
P2893410 <P2893410@GWHXX473WIN014W>
2026-05-27 23:10:23 +0100

    Staff Delete and Filter complete

--- AdminSystem/StaffConfirmDelete.aspx.cs
+++ AdminSystem/StaffConfirmDelete.aspx.cs
@@ -1,35 +1,33 @@
-﻿using ClassLibrary;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_ConfirmDelete : System.Web.UI.Page
 {
-    //variable to store the primary key value of the record to be deleted
     Int32 StaffID;
-
     protected void Page_Load(object sender, EventArgs e)
     {
-        //get the number of the address to be deleted from the session object
-        Int32 StaffID = Convert.ToInt32(Session["StaffID"]);
+        //get the staff ID to be deleted from the session object
+        StaffID = Convert.ToInt32(Session["StaffID"]);
     }
+
     protected void btnYes_Click(object sender, EventArgs e)
     {
-        //create a new instance of the staff collection class
-        clsStaffCollection Staff = new clsStaffCollection();
-        //delete the record
-        Staff.Delete();
-        //redirect back to the main page
+        clsStaffCollection AllStaff = new clsStaffCollection();
+        AllStaff.ThisStaff.Find(StaffID);
+        AllStaff.Delete();
         Response.Redirect("StaffList.aspx");
     }
+
     protected void btnNo_Click(object sender, EventArgs e)
     {
-        //redirect back to the main page
         Response.Redirect("StaffList.aspx");
     }
 }
+
 
 
