15c930c1bebc67003f3e793237a6258f5082522d
P2893410 <P2893410@GWHXX473WIN014W>
2026-06-03 11:47:21 +0100

    Almost there

--- Testing4/tstStaffCollection.cs
+++ Testing4/tstStaffCollection.cs
@@ -96,37 +96,23 @@ namespace Testing4
             [TestMethod]
             public void AddMethodOK()
             {
-                //create an instance of the class we want to create
                 clsStaffCollection AllStaff = new clsStaffCollection();
-
-                //create the item of test data
                 clsStaff TestItem = new clsStaff();
-
-                //variable to store the primary key
                 Int32 PrimaryKey = 0;
-
-                //set its properties
                 TestItem.StaffRole = "Staff Admin";
                 TestItem.StaffName = "Tyson Granger";
                 TestItem.StaffPhoneNo = "07969454221";
                 TestItem.StaffAddress = "64 Zoo Lane";
                 TestItem.StaffDateofHire = DateTime.Now;
                 TestItem.StaffClockIn = true;
-
-                //set ThisAddress to the test data
                 AllStaff.ThisStaff = TestItem;
-
-                //add the record
                 PrimaryKey = AllStaff.Add();
-
-                //set the primary key of the test data
                 TestItem.StaffID = PrimaryKey;
-
-                //find the record
                 AllStaff.ThisStaff.Find(PrimaryKey);
-
-                //test to see that the two values are the same
                 Assert.AreEqual(AllStaff.ThisStaff, TestItem);
+                //cleanup - delete the test record
+                AllStaff.ThisStaff.Find(PrimaryKey);
+                AllStaff.Delete();
             }
         }
         [TestClass]
@@ -135,41 +121,31 @@ namespace Testing4
             [TestMethod]
             public void UpdateMethodOK()
             {
-
-                //create an instance of the class we want to create
                 clsStaffCollection AllStaff = new clsStaffCollection();
-                //create the item of test data
                 clsStaff TestItem = new clsStaff();
-                //variable to store the primary key
                 Int32 PrimaryKey = 0;
-                //set its properties
                 TestItem.StaffClockIn = true;
                 TestItem.StaffRole = "Staff Admin";
                 TestItem.StaffDateofHire = DateTime.Now;
                 TestItem.StaffName = "Tyson Granger";
                 TestItem.StaffPhoneNo = "07969454221";
                 TestItem.StaffAddress = "64 Zoo Lane";
-                //set ThisStaff to the test data
                 AllStaff.ThisStaff = TestItem;
-                //add the record
                 PrimaryKey = AllStaff.Add();
-                //set the primary key of the test data
                 TestItem.StaffID = PrimaryKey;
-                //modify the test record
                 TestItem.StaffClockIn = false;
                 TestItem.StaffRole = "Manager";
                 TestItem.StaffDateofHire = DateTime.Now;
                 TestItem.StaffName = "Another Name";
                 TestItem.StaffPhoneNo = "07000000000";
                 TestItem.StaffAddress = "Another Address";
-                //set the record based on the new test data
                 AllStaff.ThisStaff = TestItem;
-                //update the record
                 AllStaff.Update();
-                //find the record
                 AllStaff.ThisStaff.Find(PrimaryKey);
-                //test to see if ThisStaff matches the test data
                 Assert.AreEqual(AllStaff.ThisStaff, TestItem);
+                //cleanup - delete the test record
+                AllStaff.ThisStaff.Find(PrimaryKey);
+                AllStaff.Delete();
             }
         }
 
