b3055f0ad08c7861497d002339757a194f686d72
P2893410 <P2893410@GWHXX473WIN014W>
2026-06-03 15:55:52 +0100

    Excuse me Sir

---
+++ ClassLibrary/clsStaffAuditLogs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaffAuditLogs
+{
+    public class clsStaffAuditLogs
+    {
+        public int StaffID { get; set; }
+        public string Action { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string PerformedBy { get; set; }
+    }
+}
