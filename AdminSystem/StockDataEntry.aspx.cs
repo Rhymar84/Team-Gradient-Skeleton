637b2ed46413943c047ae93f0ef8095f7c539d59
Dennis Ani <dennisani25@gmail.com>
2026-06-05 10:41:03 +0100

    Done and dusted I need to eat

--- AdminSystem/StockDataEntry.aspx.cs
+++ AdminSystem/StockDataEntry.aspx.cs
@@ -21,14 +21,14 @@ public partial class _1_DataEntry : System.Web.UI.Page
 
     protected void btnOK_Click(object sender, EventArgs e)
     {
-        //Create a new instance of clsStock
+        /*Create a new instance of clsStock
         clsStock anStock = new clsStock();
         //capture the item number
         anStock.itemNo = Convert.ToInt32(txtItemNo.Text);
         //Store the address in the session object
         Session["AnStock"] = anStock;
         //navigate to the view page
-        Response.Redirect("StockViewer.aspx");
+        Response.Redirect("StockViewer.aspx");*/
 
     }
 }
